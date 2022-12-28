f6a2da375484dc6ace9d0ab138b6a4892d6ecc97
dominikaguzior890 <dominika.guzior@gmail.com>
2022-12-28 18:40:14 +0100

    done

---
+++ app/src/main/java/edu/mob/mapaprojekt1/CListLocation.java
@@ -0,0 +1,14 @@
+package edu.mob.mapaprojekt1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CListLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_saved_locations_list);
+    }
+}
